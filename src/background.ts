5a8402dc65e977c34ec5acbe830d4a79e2111eac
Kamala <kamala.rocci@willapay.com>
2023-02-19 22:20:14 +0100

    Start on install + refactoring

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,6 @@
-
-const persistBrowserStartTime = () => {
-    chrome.runtime.onStartup.addListener(() => {
-        const startTime = new Date().toString();
-        chrome.storage.sync.set({"startTime": startTime });
-    });
+const storeBrowserStartTime = () => {
+    const startTime = new Date().toString();
+    chrome.storage.sync.set({"startTime": startTime });
 }
 
 const calculateActiveTime = (startTime : string)  : string => {
@@ -14,16 +11,17 @@ const calculateActiveTime = (startTime : string)  : string => {
     return Math.floor(timeDifferenceFromNow).toString();
 }
 
-const getWiseWords = () => { 
+const getStaticWiseWords = () => { 
     return [
-    `Have you been outside today? 🤨 (actually outside, not just to take out the trash or shout at the elderly upstairs neighbour who keeps blasting badly dubbed soap operas). Go touch some grass.`,
+    `Have you been outside today? 🤨 (actually outside, not just to take out the trash or shout at the elderly upstairs neighbour who keeps blasting badly dubbed soap operas).`,
     `Hello little racoon baby. Basking in the blue light as usual huh? What about getting some Vitamin D in the other light, the one that’s actually beneficial you know… 🌞`,
     `Wadup bb. I’d call you a silly goose but geese are actually hydrated: when was the last time you had a sip of water today? Sip sip sip 💦`,
     `Browser has been going bzz bzz for a long time 🔫 go have some water or I’ll show everyone your browser history next time you’re sharing your screen on a video call. I mean it. Don’t think that I can’t. Gooooo`,
+    `Hi ratty rat cutie!! 🐭 Wow long time on the internet today: good job at staring at a screen for so long! Please go touch some grass. Or look away fro a while at least.`
 ]}
 
 const pickRandomSentence = () => {
-    const wiseWords = getWiseWords();
+    const wiseWords = getStaticWiseWords();
     let wiseWordsCopy = wiseWords.slice(0);
 
     if (wiseWordsCopy.length <= 1) {
@@ -36,25 +34,22 @@ const pickRandomSentence = () => {
 }
 
 const generateNewRandomAlarm = () => {
-    const randomHourInterval = Math.floor(Math.random() * (5 - 1 + 1) + 1);
-    const randomIntervalToMinutes = randomHourInterval * 60;
-    chrome.alarms.create({ delayInMinutes: randomIntervalToMinutes });
+    const randomAmountHours = Math.floor(Math.random() * (6 - 3 + 1) + 3);
+    const randomAmountMinutes = randomAmountHours * 60;
+    chrome.alarms.create({ delayInMinutes: randomAmountMinutes });
 }
 
 const triggerNotificationOnAlarm = () => {
-    chrome.alarms.onAlarm.addListener(() => {     
-        chrome.storage.sync.get(["startTime"], function(storedDate){
-            const activeTime = calculateActiveTime(storedDate.startTime);
-            
-            if(activeTime !== 'NaN') {
-                chrome.notifications.create('smth', {
-                    title: `Browser open since ${calculateActiveTime(storedDate.startTime)} hours`,
-                    message: pickRandomSentence(),
-                    iconUrl: 'bestie.png',
-                    type: 'basic',
-                    priority: 2,
-                });
-            }
+    chrome.alarms.onAlarm.addListener(async () => {  
+        const storedDate = await chrome.storage.sync.get(["startTime"]);   
+        const activeTime = calculateActiveTime(storedDate.startTime);
+
+        chrome.notifications.create('smth', {
+            title: `Browser open since ${activeTime} hours`,
+            message: pickRandomSentence(),
+            iconUrl: 'bestie.png',
+            type: 'basic',
+            priority: 2,
         });
     });
     chrome.notifications.onClosed.addListener(() => {
@@ -62,8 +57,8 @@ const triggerNotificationOnAlarm = () => {
     });
 }
 
-async function main() {
-    persistBrowserStartTime();
+function main() {
+    storeBrowserStartTime();
     generateNewRandomAlarm();
     triggerNotificationOnAlarm();
 }
